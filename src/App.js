a128fbc8d2fec9fa75143a319e48fe6ac4b1c59f
Felipe Vieira <felipe.svfx@gmail.com>
2020-01-24 21:14:40 -0200

    React basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Switch from "./components/Switch";
 
-function App() {
+const initialState = {
+  fat: false,
+  bald: false,
+  poor: false
+};
+
+const App = () => {
+  const [switches, setSwitches] = useState(initialState);
+
+  const toggleSwitch = id => {
+    const currentState = switches[id];
+    if (currentState === undefined) return;
+
+    const activeSwitches = Object.values(switches).filter(value => value)
+      .length;
+
+    const maxActive = 2;
+
+    if (activeSwitches === maxActive) {
+      setSwitches({ ...initialState, [id]: !currentState });
+    } else {
+      setSwitches({ ...switches, [id]: !currentState });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Gordo, Careca ou Pobre</h1>
+      <Switch
+        onClick={toggleSwitch}
+        id="fat"
+        active={switches["fat"]}
+        label="Gordo"
+      />
+      <Switch
+        onClick={toggleSwitch}
+        id="bald"
+        active={switches["bald"]}
+        label="Careca"
+      />
+      <Switch
+        onClick={toggleSwitch}
+        id="poor"
+        active={switches["poor"]}
+        label="Pobre"
+      />
     </div>
   );
-}
+};
 
 export default App;
